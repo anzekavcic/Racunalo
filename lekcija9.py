5dfe0cff03e8a39c82c8a9a32c4df55c2a9fad51
Anže Kavčič <anzekavcic@Anzes-MBP.dlabs.lan>
2016-03-21 18:33:08 +0100

    Racunalo

---
+++ lekcija9.py
@@ -0,0 +1,16 @@
+
+
+a = float(raw_input("vpisi prvo stevilko: "))
+b = float(raw_input("vpisi drugo stevilko: "))
+operacija = raw_input("izberi operacijo: ")
+if operacija == "sestevanje" :
+    print a + b
+
+elif operacija =="odstevanje" :
+    print a - b
+
+elif operacija =="mnozenje" :
+    print a * b
+
+elif operacija == "deljenje" :
+    print a / b
